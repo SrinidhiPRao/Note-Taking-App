4a5e98b55db6a8f36442166a12e523746b36e711
Srinidhi Rao <srinidhi.prao@gmail.com>
2024-08-05 23:29:41 +0530

    Added Note Taking System

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,7 @@ from .form_val import check_email, check_name, check_password
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint("auth", __name__)
@@ -12,22 +13,25 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
-    
+
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in Successfully!", category="success")
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash("Incorrect Password", category="error")
         else:
             flash("User doesn't exist.", category= "error")
         
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
    
 @auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
@@ -40,7 +44,7 @@ def sign_up():
         # Refactor this code in the future
         user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already in use', category='error')
+            flash('Email already in use.', category='error')
         elif not check_email(email):
             flash("Invalid Email", category='error')
         elif not check_name(firstname):
@@ -54,4 +58,4 @@ def sign_up():
             flash("Account Created", category="success")
             return redirect(url_for('views.home'))
         
-    return render_template("signup.html")
+    return render_template("signup.html", user = current_user)
